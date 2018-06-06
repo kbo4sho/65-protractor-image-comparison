754dd53afcb5d08c2f1779edd14cea326479a4ea
Bolander <kbolander@CHI-KBOLAND-MAC.local>
2018-06-06 11:38:02 -0500

    error reproduction

--- example/e2e/protractor.conf.js
+++ example/e2e/protractor.conf.js
@@ -4,25 +4,35 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.e2e.json')
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+    allScriptsTimeout: 11000,
+    specs: [
+        './src/**/*.e2e-spec.ts'
+    ],
+    capabilities: {
+        'browserName': 'chrome',
+        chromeOptions: {
+            args: [
+                '--no-sandbox',
+                '--headless',
+                '--disable-web-security',
+                '--disable-gpu',
+                '--disable-dev-shm-usage',
+                '--window-size=1366,768'
+            ]
+        }
+    },
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function () { }
+    },
+    onPrepare() {
+        require('ts-node').register({
+            project: require('path').join(__dirname, './tsconfig.e2e.json')
+        });
+        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    }
 };
--- example/e2e/src/app.e2e-spec.ts
+++ example/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,21 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
+const protractorImageComparison = require('protractor-image-comparison');
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
+    browser.protractorImageComparison = new protractorImageComparison({
+        baselineFolder: './e2e/h1/vrt/baseline/',
+        screenshotPath: './e2e/h1/vrt/.tmp/',
+        autoSaveBaseline: true
+    });
+    browser.get('/');
     page = new AppPage();
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to example!');
+    expect(browser.protractorImageComparison.checkElement(page.getH1(), 'h1')).toEqual(0);
   });
 });
